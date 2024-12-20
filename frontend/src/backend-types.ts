0fd49652cd276b8aa6875a5d31c910ac645489e7
Krzysiek <hisboak@gmail.com>
2024-12-20 13:42:43 +0100

    Synchronized types with backend

--- frontend/src/backend-types.ts
+++ frontend/src/backend-types.ts
@@ -3,3 +3,5 @@ import type { components } from "./../schema";
 export type UserPublic = components["schemas"]["UserPublic"];
 
 export type ExecutionTemplatePublic = components["schemas"]["ExecutionTemplatePublic"];
+
+export type JobPublic = components["schemas"]["JobPublic"]
